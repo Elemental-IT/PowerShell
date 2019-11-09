250f04f465e2fc9be29948a1857a29a19b19c4b7
Bedlem55 <52682333+Bedlem55@users.noreply.github.com>
2019-11-09 20:35:23 +1100

    Delete PS6_Installer.ps1

--- PS6_Installer.ps1
+++
@@ -1,8 +0,0 @@
-$Link = "https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/PowerShell-6.2.0-win-x64.msi"
-$Path = "C:\temp\PS6\PS6.msi"
-$Arguments = "/quiet /norestart"
-
-New-Item -Path "C:\temp\PS6" -ItemType Directory -Force -ErrorAction SilentlyContinue
-(New-Object Net.WebClient).DownloadFile($Link, $path)
-
-Start-Process -FilePath $Path -ArgumentList $Arguments -Wait
