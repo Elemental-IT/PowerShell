f9e0149ecf103ef8298e8c23c3ceed9d3d5ee60c
Bedlem55 <52682333+Bedlem55@users.noreply.github.com>
2019-12-06 15:46:24 +1100

    Added installer for system voices

--- Text to Wave/Text to Wave.ps1
+++ Text to Wave/Text to Wave.ps1
@@ -1,4 +1,12 @@
-﻿
+<#
+.SYNOPSIS
+  Text to voice app
+.DESCRIPTION
+  A GUI for windows speechsynthesis  
+.NOTES
+  Author Theo bird
+#>
+
 # Assembly
 #==========================================
 
@@ -11,9 +19,8 @@ Add-Type -AssemblyName System.Windows.Forms
 # VariableS
 #===========================================================
 
-$style =  "flat"
+$style = "flat"
 $FormBackColor = "DodgerBlue"
-$ButtionForeColor = "Black"
 $ButtionBackColor = "Aqua"
 
 $About = @'
@@ -26,53 +33,210 @@ $About = @'
 '@
 
 
+$Eva = @'
+Windows Registry Editor Version 5.00
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech_OneCore\Voices\Tokens\MSTTS_V110_enUS_EvaM]
+@="Microsoft Eva Mobile - English (United States)"
+"LangDataPath"="%windir%\\Speech_OneCore\\Engines\\TTS\\en-US\\MSTTSLocenUS.dat"
+"LangUpdateDataDirectory"="%SystemDrive%\\Data\\SharedData\\Speech_OneCore\\Engines\\TTS\\en-US"
+"VoicePath"="%windir%\\Speech_OneCore\\Engines\\TTS\\en-US\\M1033Eva"
+"VoiceUpdateDataDirectory"="%SystemDrive%\\Data\\SharedData\\Speech_OneCore\\Engines\\TTS\\en-US"
+"409"="Microsoft Eva Mobile - English (United States)"
+"CLSID"="{179F3D56-1B0B-42B2-A962-59B7EF59FE1B}"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech_OneCore\Voices\Tokens\MSTTS_V110_enUS_EvaM\Attributes]
+"Version"="11.0"
+"Language"="409"
+"Gender"="Female"
+"Age"="Adult"
+"DataVersion"="11.0.2013.1022"
+"SharedPronunciation"=""
+"Name"="Microsoft Eva Mobile"
+"Vendor"="Microsoft"
+"PersonalAssistant"="1"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_enUS_EvaM]
+@="Microsoft Eva Mobile - English (United States)"
+"LangDataPath"="%windir%\\Speech_OneCore\\Engines\\TTS\\en-US\\MSTTSLocenUS.dat"
+"LangUpdateDataDirectory"="%SystemDrive%\\Data\\SharedData\\Speech_OneCore\\Engines\\TTS\\en-US"
+"VoicePath"="%windir%\\Speech_OneCore\\Engines\\TTS\\en-US\\M1033Eva"
+"VoiceUpdateDataDirectory"="%SystemDrive%\\Data\\SharedData\\Speech_OneCore\\Engines\\TTS\\en-US"
+"409"="Microsoft Eva Mobile - English (United States)"
+"CLSID"="{179F3D56-1B0B-42B2-A962-59B7EF59FE1B}"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_enUS_EvaM\Attributes]
+"Version"="11.0"
+"Language"="409"
+"Gender"="Female"
+"Age"="Adult"
+"DataVersion"="11.0.2013.1022"
+"SharedPronunciation"=""
+"Name"="Microsoft Eva Mobile"
+"Vendor"="Microsoft"
+"PersonalAssistant"="1"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\SPEECH\Voices\Tokens\MSTTS_V110_enUS_EvaM]
+@="Microsoft Eva Mobile - English (United States)"
+"LangDataPath"="%windir%\\Speech_OneCore\\Engines\\TTS\\en-US\\MSTTSLocenUS.dat"
+"LangUpdateDataDirectory"="%SystemDrive%\\Data\\SharedData\\Speech_OneCore\\Engines\\TTS\\en-US"
+"VoicePath"="%windir%\\Speech_OneCore\\Engines\\TTS\\en-US\\M1033Eva"
+"VoiceUpdateDataDirectory"="%SystemDrive%\\Data\\SharedData\\Speech_OneCore\\Engines\\TTS\\en-US"
+"409"="Microsoft Eva Mobile - English (United States)"
+"CLSID"="{179F3D56-1B0B-42B2-A962-59B7EF59FE1B}"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\SPEECH\Voices\Tokens\MSTTS_V110_enUS_EvaM\Attributes]
+"Version"="11.0"
+"Language"="409"
+"Gender"="Female"
+"Age"="Adult"
+"DataVersion"="11.0.2013.1022"
+"SharedPronunciation"=""
+"Name"="Microsoft Eva Mobile"
+"Vendor"="Microsoft"
+"PersonalAssistant"="1"
+'@
+
+$Mark = @'
+Windows Registry Editor Version 5.00
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_enUS_MarkM]
+@="Microsoft Mark - English (United States)"
+"409"="Microsoft Mark - English (United States)"
+"CLSID"="{179F3D56-1B0B-42B2-A962-59B7EF59FE1B}"
+"LangDataPath"=hex(2):25,00,77,00,69,00,6e,00,64,00,69,00,72,00,25,00,5c,00,53,\
+  00,70,00,65,00,65,00,63,00,68,00,5f,00,4f,00,6e,00,65,00,43,00,6f,00,72,00,\
+  65,00,5c,00,45,00,6e,00,67,00,69,00,6e,00,65,00,73,00,5c,00,54,00,54,00,53,\
+  00,5c,00,65,00,6e,00,2d,00,55,00,53,00,5c,00,4d,00,53,00,54,00,54,00,53,00,\
+  4c,00,6f,00,63,00,65,00,6e,00,55,00,53,00,2e,00,64,00,61,00,74,00,00,00
+"VoicePath"=hex(2):25,00,77,00,69,00,6e,00,64,00,69,00,72,00,25,00,5c,00,53,00,\
+  70,00,65,00,65,00,63,00,68,00,5f,00,4f,00,6e,00,65,00,43,00,6f,00,72,00,65,\
+  00,5c,00,45,00,6e,00,67,00,69,00,6e,00,65,00,73,00,5c,00,54,00,54,00,53,00,\
+  5c,00,65,00,6e,00,2d,00,55,00,53,00,5c,00,4d,00,31,00,30,00,33,00,33,00,4d,\
+  00,61,00,72,00,6b,00,00,00
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_enUS_MarkM\Attributes]
+"Age"="Adult"
+"DataVersion"="11.0.2013.1022"
+"Gender"="Male"
+"Language"="409"
+"Name"="Microsoft Mark"
+"SharedPronunciation"=""
+"Vendor"="Microsoft"
+"Version"="11.0"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\SPEECH\Voices\Tokens\MSTTS_V110_enUS_MarkM]
+@="Microsoft Mark - English (United States)"
+"409"="Microsoft Mark - English (United States)"
+"CLSID"="{179F3D56-1B0B-42B2-A962-59B7EF59FE1B}"
+"LangDataPath"=hex(2):25,00,77,00,69,00,6e,00,64,00,69,00,72,00,25,00,5c,00,53,\
+  00,70,00,65,00,65,00,63,00,68,00,5f,00,4f,00,6e,00,65,00,43,00,6f,00,72,00,\
+  65,00,5c,00,45,00,6e,00,67,00,69,00,6e,00,65,00,73,00,5c,00,54,00,54,00,53,\
+  00,5c,00,65,00,6e,00,2d,00,55,00,53,00,5c,00,4d,00,53,00,54,00,54,00,53,00,\
+  4c,00,6f,00,63,00,65,00,6e,00,55,00,53,00,2e,00,64,00,61,00,74,00,00,00
+"VoicePath"=hex(2):25,00,77,00,69,00,6e,00,64,00,69,00,72,00,25,00,5c,00,53,00,\
+  70,00,65,00,65,00,63,00,68,00,5f,00,4f,00,6e,00,65,00,43,00,6f,00,72,00,65,\
+  00,5c,00,45,00,6e,00,67,00,69,00,6e,00,65,00,73,00,5c,00,54,00,54,00,53,00,\
+  5c,00,65,00,6e,00,2d,00,55,00,53,00,5c,00,4d,00,31,00,30,00,33,00,33,00,4d,\
+  00,61,00,72,00,6b,00,00,00
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\SPEECH\Voices\Tokens\MSTTS_V110_enUS_MarkM\Attributes]
+"Age"="Adult"
+"DataVersion"="11.0.2013.1022"
+"Gender"="Male"
+"Language"="409"
+"Name"="Microsoft Mark"
+"SharedPronunciation"=""
+"Vendor"="Microsoft"
+"Version"="11.0"
+'@
+
+$Message = @'
+Enable Eva and Mark Voices? 
+
+You will need to restart this after enabing 
+
+Warning: this is modify the system registry.
+'@
+
+$OS = @'
+OS does not meet requirements:
+
+Windows 10 or Server 2016 and higher.
+'@
+
 # Base Form
 #==========================================
 
 Function PlaySound {
 
-if ($SelectVoiceCB.SelectedItem  -eq $null){
-    [System.Windows.Forms.MessageBox]::Show("No voice selected","    Warning:") 
- } Else {
+  if ($null -eq $SelectVoiceCB.SelectedItem) {
+    [System.Windows.Forms.MessageBox]::Show("No voice selected", "    Warning:") 
+  }
+  Else {
     $speak.SetOutputToDefaultAudioDevice() ; 
     $Speak.Rate = ($speed.Value)
     $Speak.Volume = $Volume.Value 
     $speak.SelectVoice($SelectVoiceCB.Text) 
     $speak.Speak($SpeakTextBox.Text)
-    } 
+  } 
 }
 
 
 Function SaveSound {
-if ($SelectVoiceCB.SelectedItem  -eq $null){
-      [System.Windows.Forms.MessageBox]::Show("No voice selected","    Warning:") 
-    } else {
-      $SaveChooser = New-Object -TypeName System.Windows.Forms.SaveFileDialog
-      $SaveChooser.Title = "Save text to Wav file"
-      $SaveChooser.FileName = "SpeechSynthesizer"
-      $SaveChooser.Filter = 'Wave file (.wav) | *.wav'
-        $Answer = $SaveChooser.ShowDialog(); $Answer
+  if ($null -eq $SelectVoiceCB.SelectedItem) {
+    [System.Windows.Forms.MessageBox]::Show("No voice selected", "    Warning:") 
+  }
+  else {
+    $SaveChooser = New-Object -TypeName System.Windows.Forms.SaveFileDialog
+    $SaveChooser.Title = "Save text to Wav file"
+    $SaveChooser.FileName = "SpeechSynthesizer"
+    $SaveChooser.Filter = 'Wave file (.wav) | *.wav'
+    $Answer = $SaveChooser.ShowDialog(); $Answer
 
-      if( $Answer -eq "OK" ) {
-           $speak.SetOutputToDefaultAudioDevice() ; 
-        $Script:Speak.Rate = ($speed.Value)
-        $Speak.Volume = $Volume.Value 
-        $speak.SelectVoice($SelectVoiceCB.Text) 
-                $speak.SetOutputToWaveFile($SaveChooser.Filename)
-          $speak.Speak($SpeakTextBox.Text)
-          $speak.SetOutputToNull()
-          $speak.SpeakAsyncCancelAll()
-          }
-     }
+    if ( $Answer -eq "OK" ) {
+      $speak.SetOutputToDefaultAudioDevice() ; 
+      $Script:Speak.Rate = ($speed.Value)
+      $Speak.Volume = $Volume.Value 
+      $speak.SelectVoice($SelectVoiceCB.Text) 
+      $speak.SetOutputToWaveFile($SaveChooser.Filename)
+      $speak.Speak($SpeakTextBox.Text)
+      $speak.SetOutputToNull()
+      $speak.SpeakAsyncCancelAll()
+    }
+  }
 }
+
+Function EnableEvaMark { 
+  
+  if (-not(Get-WmiObject -Class win32_operatingsystem).version.remove(2) -eq 10 ) { 
+    [System.Windows.Forms.MessageBox]::Show("$OS,    Warning:") 
+  }
+  else {
+
+    $UserPrompt = new-object -comobject wscript.shell
+    $Answer = $UserPrompt.popup($Message, 0, "Enable system Voices", 4)
+
+    If ($Answer -eq 6) {
+      New-Item -Value $eva -Path $env:SystemDrive\Temp\Eva.reg
+      New-Item -Value $Mark -Path $env:SystemDrive\Temp\Mark.reg
+      Invoke-Item $env:SystemDrive\Temp\Eva.reg 
+      Invoke-Item $env:SystemDrive\Temp\Mark.reg
+    }
+    else { $null }
+  }
+
+}
+
+
+
 
 # Base Form
 #==========================================
 
-
 $Form = New-Object system.Windows.Forms.Form
 $Form.ClientSize = '798,525'
-$Form.MinimumSize  = '815,570'
+$Form.MinimumSize = '815,570'
 $Form.text = "Text to Wave"
 $Form.BackColor = $FormBackColor
 $Form.ShowIcon = $false
@@ -81,34 +245,74 @@ $Form.TopMost = $false
 # Menu
 #==========================================
 
-$Menu  = New-Object System.Windows.Forms.MenuStrip
+$Menu = New-Object System.Windows.Forms.MenuStrip
 
-$MenuFile  = New-Object System.Windows.Forms.ToolStripMenuItem
-    $MenuFile.Text = "&File"
-    [void]$Menu.Items.Add($MenuFile)
+$MenuFile = New-Object System.Windows.Forms.ToolStripMenuItem
+$MenuFile.Text = "&File"
+[void]$Menu.Items.Add($MenuFile)
 
-        $MenuExit  = New-Object System.Windows.Forms.ToolStripMenuItem
-        $MenuExit.Text = "&Exit"
-        $menuExit.Add_Click({ $Form.close() })
+$MenuExit = New-Object System.Windows.Forms.ToolStripMenuItem
+$MenuExit.Text = "&Exit"
+$menuExit.Add_Click( { $Form.close() })
 [void]$MenuFile.DropDownItems.Add($MenuExit)
 
-$MenuHelp  = New-Object System.Windows.Forms.ToolStripMenuItem
-    $MenuHelp.Text = "&Help"
-    [void]$Menu.Items.Add($MenuHelp)
 
-        $MenuAbout  = New-Object System.Windows.Forms.ToolStripMenuItem
-        $MenuAbout.Text = "&About"
-        $MenuAbout.Add_Click({ [System.Windows.Forms.MessageBox]::Show("$About","    About") })
+$MenuVoices = New-Object System.Windows.Forms.ToolStripMenuItem
+$MenuVoices.Text = "&Voice"
+[void]$Menu.Items.Add($MenuVoices)
+
+$InstallVoices = New-Object System.Windows.Forms.ToolStripMenuItem
+$InstallVoices.Text = "&Enable Eva and Mark"
+$InstallVoices.Add_Click( { EnableEvaMark })
+[void]$MenuVoices.DropDownItems.Add($InstallVoices)
+
+
+$MenuHelp = New-Object System.Windows.Forms.ToolStripMenuItem
+$MenuHelp.Text = "&Help"
+[void]$Menu.Items.Add($MenuHelp)
+
+$MenuAbout = New-Object System.Windows.Forms.ToolStripMenuItem
+$MenuAbout.Text = "&About"
+$MenuAbout.Add_Click( { [System.Windows.Forms.MessageBox]::Show("$About", "    About") })
 [void]$MenuHelp.DropDownItems.Add($MenuAbout)
 
-# Base Form Objects
+$SpeakButtion = New-Object system.Windows.Forms.Button
+$SpeakButtion.location = "660, 401"
+$SpeakButtion.Size = "127, 43"
+$SpeakButtion.Anchor = "Bottom"
+$SpeakButtion.text = "Play"
+$SpeakButtion.BackColor = $ButtionBackColor
+$SpeakButtion.FlatStyle = $style
+$SpeakButtion.FlatAppearance.BorderSize = '0'
+$SpeakButtion.Font = 'Microsoft Sans Serif,10'
+$SpeakButtion.add_Click( { PlaySound })
+
+$SaveButtion = New-Object system.Windows.Forms.Button
+$SaveButtion.location = "660, 456"
+$SaveButtion.Size = "127, 55"
+$SaveButtion.Anchor = "Bottom"
+$SaveButtion.text = "Save"
+$SaveButtion.BackColor = $ButtionBackColor
+$SaveButtion.FlatStyle = $style
+$SaveButtion.FlatAppearance.BorderSize = '0'
+$SaveButtion.Font = 'Microsoft Sans Serif,10'
+$SaveButtion.add_Click( { SaveSound })
+
+# Text Group Box
 #==========================================
 
+$TextGB = New-Object system.Windows.Forms.Groupbox
+$TextGB.Anchor = "Top, Bottom, Left, Right"
+$TextGB.location = "10, 35"
+$TextGB.Size = "775, 350"
+$TextGB.text = "Enter or Drag Text here"
+$TextGB.ForeColor = "white"
+
 $SpeakTextBox = New-Object System.Windows.Forms.RichTextBox
-$SpeakTextBox.location = "12, 35"
-$SpeakTextBox.Size = "775, 350"
+$SpeakTextBox.location = "10, 15"
+$SpeakTextBox.Size = "755, 325"
 $SpeakTextBox.Anchor = "Top, Bottom, Left, Right"
-$SpeakTextBox.Text = "Enter text here"
+$SpeakTextBox.Text = "Hello World"
 $speakTextbox.AllowDrop = $true
 $speakTextbox.EnableAutoDragDrop = $true
 $SpeakTextBox.multiline = $true
@@ -117,29 +321,7 @@ $SpeakTextBox.ScrollBars = "both"
 $SpeakTextBox.BorderStyle = "None"
 $SpeakTextBox.Font = 'Microsoft Sans Serif,10'
 $SpeakTextBox.Cursor = "IBeam"
-$Form.Controls.Add($SpeakTextBox)
-
-$SpeakButtion = New-Object system.Windows.Forms.Button
-$SpeakButtion.location = "660, 400"
-$SpeakButtion.Size = "127, 45"
-$SpeakButtion.Anchor = "Bottom"
-$SpeakButtion.text = "Play"
-$SpeakButtion.BackColor = $ButtionBackColor
-$SpeakButtion.FlatStyle = $style
-$SpeakButtion.FlatAppearance.BorderSize = '0'
-$SpeakButtion.Font = 'Microsoft Sans Serif,10'
-$SpeakButtion.add_Click({ PlaySound })
-
-$SaveButtion = New-Object system.Windows.Forms.Button
-$SaveButtion.location = "660, 465"
-$SaveButtion.Size = "127, 45"
-$SaveButtion.Anchor = "Bottom"
-$SaveButtion.text = "Save"
-$SaveButtion.BackColor = $ButtionBackColor
-$SaveButtion.FlatStyle = $style
-$SaveButtion.FlatAppearance.BorderSize = '0'
-$SaveButtion.Font = 'Microsoft Sans Serif,10'
-$SaveButtion.add_Click({ SaveSound })
+$TextGB.Controls.Add( $SpeakTextBox )
 
 # Select Group Box
 #==========================================
@@ -177,10 +359,10 @@ $SpeedGB.ForeColor = "white"
 
 $Speed = New-Object Windows.Forms.TrackBar
 $Speed.Orientation = "Horizontal"
-$Speed.location  = "5,15"
+$Speed.location = "5,15"
 $Speed.Size = "300,40"
 $Speed.TickStyle = "TopLeft"
-$Speed.SetRange(-10,10)
+$Speed.SetRange(-10, 10)
 $SpeedGB.Controls.Add( $Speed )
 
 # Volume Group Box
@@ -188,24 +370,24 @@ $SpeedGB.Controls.Add( $Speed )
 
 $VolumeGB = New-Object system.Windows.Forms.Groupbox
 $VolumeGB.location = "340, 450"
-$VolumeGB.Size = "312,62"
+$VolumeGB.Size = "311,62"
 $VolumeGB.Anchor = "Bottom"
 $VolumeGB.text = "Volume"
 $VolumeGB.ForeColor = "white"
 
 $Volume = New-Object Windows.Forms.TrackBar
 $Volume.Orientation = "Horizontal"
-$Volume.location  = "5,15"
+$Volume.location = "5,15"
 $Volume.Size = "300,40"
 $Volume.TickStyle = "TopLeft"
 $Volume.TickFrequency = 10
-$Volume.SetRange(10,100)
+$Volume.SetRange(10, 100)
 $Volume.Value = 100
 $VolumeGB.Controls.Add( $Volume )
 
 # Controls
 #==========================================
 
-$Form.controls.AddRange(@( $Menu, $SpeechGB, $SpeakTextBox, $SpeakButtion, $SaveButtion, $SelectGB, $SpeedGB, $VolumeGB ))
+$Form.controls.AddRange(@( $Menu, $SpeechGB, $SpeakButtion, $SaveButtion, $SelectGB, $SpeedGB, $VolumeGB, $TextGB ))
 
 [void]$form.ShowDialog()
