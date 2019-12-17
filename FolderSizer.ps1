c4fda427afbbc7ee275ac0819335c6c63a361503
Bedlem55 <52682333+Bedlem55@users.noreply.github.com>
2019-12-17 15:32:39 +1100

    FolderSizer
    FolderSizer gets all system drive folder sizes and exports the list to CSV

---
+++ FolderSizer.ps1
@@ -0,0 +1,18 @@
+﻿$Exportlist = @()
+
+Set-Location $env:SystemDrive\
+
+Get-ChildItem -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.PSIsContainer } | ForEach-Object {
+
+$obj = New-Object PSObject -Property @{
+
+   Fullname = $_.FullName 
+   Size = "{0} MB" -f [math]::round(((Get-ChildItem  $_.FullName -Recurse -ErrorAction SilentlyContinue |  Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue).Sum / 1MB))
+   CreationTime = $_.CreationTime
+   LastAccessTime = $_.LastAccessTime
+      
+   } 
+   $Exportlist += $obj
+} 
+
+$Exportlist | Select-Object Fullname,Size,CreationTime,LastAccessTime | Export-Csv "$env:SystemDrive\$env:COMPUTERNAME`_foldrsize.csv" -NoTypeInformation
