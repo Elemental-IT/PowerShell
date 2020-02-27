20678b5785cf6f1980c8a7820ca7b7ad64e91272
Bedlem55 <52682333+Bedlem55@users.noreply.github.com>
2020-02-28 10:46:16 +1100

    Add files via upload

---
+++ All_Server_RDP_Setup.ps1
@@ -0,0 +1,64 @@
+﻿$Servers = (get-adcomputer -LDAPFilter "(&(objectCategory=computer)(operatingSystem=Windows Server*)(!serviceprincipalname=*MSClusterVirtualServer*)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))").Samaccountname
+$RDP_Folder = "$env:USERPROFILE\desktop\RDP_DIR"
+$Credential = $host.ui.PromptForCredential("Need credentials", "Please enter your user name and password.", "", "NetBiosUserName")
+$User = $Credential.username
+
+if ((Test-Path $RDP_Folder) -eq $false) {New-Item $RDP_Folder -Type Directory -Force}
+
+foreach($Server in $servers){
+
+$server = $server.Substring(0,$server.Length-1)
+
+$Content = @"
+screen mode id:i:2
+use multimon:i:0
+desktopwidth:i:1920
+desktopheight:i:1080
+session bpp:i:32
+winposstr:s:0,1,0,0,800,600
+compression:i:1
+keyboardhook:i:2
+audiocapturemode:i:0
+videoplaybackmode:i:1
+connection type:i:7
+networkautodetect:i:1
+bandwidthautodetect:i:1
+displayconnectionbar:i:1
+enableworkspacereconnect:i:0
+disable wallpaper:i:0
+allow font smoothing:i:0
+allow desktop composition:i:0
+disable full window drag:i:1
+disable menu anims:i:1
+disable themes:i:0
+disable cursor setting:i:0
+bitmapcachepersistenable:i:1
+full address:s:$server
+audiomode:i:0
+redirectprinters:i:1
+redirectcomports:i:0
+redirectsmartcards:i:1
+redirectclipboard:i:1
+redirectposdevices:i:0
+autoreconnection enabled:i:1
+authentication level:i:2
+prompt for credentials:i:0
+negotiate security layer:i:1
+remoteapplicationmode:i:0
+alternate shell:s:
+shell working directory:s:
+gatewayhostname:s:
+gatewayusagemethod:i:4
+gatewaycredentialssource:i:4
+gatewayprofileusagemethod:i:0
+promptcredentialonce:i:0
+gatewaybrokeringtype:i:0
+use redirection server name:i:0
+rdgiskdcproxy:i:0
+kdcproxyname:s:
+username:s:$User
+"@
+
+New-Item -Path "$RDP_Folder\$Server.RDP"
+Add-Content "$RDP_Folder\$Server.RDP" -Value $Content
+ }
