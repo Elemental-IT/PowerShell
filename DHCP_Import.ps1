8e998f47450a64d30cac5f8c5c73d617d24c4ac1
Bedlem55 <52682333+Bedlem55@users.noreply.github.com>
2021-09-23 15:26:51 +1000

    Add files via upload
    Export DHCP server scope to CSV file

---
+++ DHCP_Import.ps1
@@ -0,0 +1,9 @@
+﻿$CSV = ""
+$Scopes = Import-Csv $CSV
+$Server = ""
+
+Foreach ($Scope in $Scopes) {
+
+Add-DhcpServerv4Scope -ComputerName $Server -Name $Scope.name -Description $Scope.Description -SubnetMask $Scope.SubnetMask -StartRange $Scope.StartRange -EndRange $Scope.EndRange
+
+}
